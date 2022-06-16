07614384a05e94bb2eca80dfbf46154cf18eaf0d
raveesh_gowda_k <raveesh512@gmail.com>
2022-06-16 09:29:38 +0530

    completed login test

--- login-test/src/App.test.js
+++ login-test/src/App.test.js
@@ -1,16 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import {render, screen} from "@testing-library/react";
 import App from "./App";
-import Login from "./components/Login";
 
 describe("Test the App Component", () => {
-  test("header renders with react testing tutorial in the document", () => {
-    const component = render(<App />);
-    const linkElement = component.getByText(/This is React Testing Tutorial/i);
-    expect(linkElement).toBeInTheDocument();
-  });
-  test("render login component in doucment", () => {
-    const { getByLabelText } = render(<App />);
-    const childElement = getByLabelText("Email");
-    expect(childElement).toBeTruthy();
-  });
+	test("header renders with react testing tutorial in the document", () => {
+		render(<App />);
+		const linkElement = screen.getByText(/This is React Testing Tutorial/i);
+		expect(linkElement).toBeInTheDocument();
+	});
+	test("render login component in document", () => {
+		render(<App />);
+		const childElement = screen.getByLabelText("Email");
+		expect(childElement).toBeTruthy();
+	});
 });
